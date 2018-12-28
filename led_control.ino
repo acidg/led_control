578102b92f71b03c0ed184a0c55b0f95bf37d369
Nils Kunze <kunze@cqse.eu>
2018-12-28 18:54:46 +0100

    add acceleration sensor prototype

--- led_control.ino
+++ led_control.ino
@@ -2,12 +2,22 @@
 #include <cRGB.h>
 #include <WS2812.h>
 #include <EEPROM.h>
+#include <BMA020.h>
 
-#define PIN 10  // Digital output pin (default: 7)
+#define PIN 9  // Digital output pin (default: 7)
 #define LED_COUNT 30   // Number of LEDs to drive (default: 9)
 
 const uint8_t curve[16] PROGMEM = {
     1, 2, 3, 4, 6, 8, 11, 16, 22, 32, 45, 63, 90, 127, 180, 255
+};
+
+/* eprom save locations */
+enum eprom {
+    EPROM_MODE = 0,
+    EPROM_SPEED = 1,
+    EPROM_R = 2,
+    EPROM_G = 3,
+    EPROM_B = 4
 };
 
 /* Available patterns */
@@ -17,7 +27,8 @@ enum modes {
     MODE_COLOR = 2,
     MODE_FADE_COLOR = 3,
     MODE_RUNNING_COLOR = 4,
-    MODE_RUNNING_PARALLEL = 5
+    MODE_RUNNING_PARALLEL = 5,
+    MODE_LEFT_RIGHT_TEST = 6
 };
 
 /* The three main commands for adjusting the mode, speed or color */
@@ -37,16 +48,19 @@ cRGB current_color;
 
 /* Executed once during Arduino startup */
 void setup() {
+    // Starte I2C verbindung und setzte Messbereich auf 2g
+    BMA::begin(RANGE_2g, BANDWIDTH_25hz); 
+
     Serial.begin(9600);
     Serial.println("LED Control");
     randomSeed(analogRead(0));
     led.setOutput(PIN);
 
-    current_mode = EEPROM.read(0);
-    current_speed = EEPROM.read(1);
-    current_color.r = EEPROM.read(2);
-    current_color.g = EEPROM.read(3);
-    current_color.b = EEPROM.read(4);
+    current_mode = EEPROM.read(EPROM_MODE);
+    current_speed = EEPROM.read(EPROM_SPEED);
+    current_color.r = EEPROM.read(EPROM_R);
+    current_color.g = EEPROM.read(EPROM_G);
+    current_color.b = EEPROM.read(EPROM_B);
     printMode();
 }
 
@@ -68,7 +82,7 @@ void loop() {
             setAllLED(current_color);
             break;
         case MODE_FADE_COLOR:
-            //fadeColor();
+            fadeColor(current_color, current_speed);
             break;
         case MODE_RUNNING_COLOR:
             running_color();
@@ -76,43 +90,55 @@ void loop() {
         case MODE_RUNNING_PARALLEL:
             running_parallel();
             break;
+        case MODE_LEFT_RIGHT_TEST:
+            left_right_test();
+            break;
         default:
             powerDown();
             break;
     }
 }
 
+int read_serial() {
+    while(!Serial.available());
+    int tmp = Serial.parseInt();
+    Serial.print(tmp);
+    return tmp;
+}
+
 void readCommand() {
-    int command = Serial.parseInt();
+    Serial.print("Change Mode(0), Speed(1) or Color(2)? ");
+    int command = read_serial();
+    Serial.println();
+
     switch (command) {
         case COMMAND_MODE:
-            Serial.println("Mode?");
-            while(!Serial.available());
-            current_mode = Serial.parseInt();
-            EEPROM.write(0, current_mode);
-            Serial.println(current_mode);
+            Serial.print("Mode? ");
+            current_mode = read_serial();
+            EEPROM.write(EPROM_MODE, current_mode);
+            Serial.println();
             break;
         case COMMAND_SPEED:
-            Serial.println("Speed?");
-            while(!Serial.available());
-            current_speed = Serial.parseInt();
-            Serial.println(current_speed);
-            EEPROM.write(1, current_speed);
+            Serial.print("Speed? ");
+            current_speed = read_serial();
+            EEPROM.write(EPROM_SPEED, current_speed);
+            Serial.println();
             break;
         case COMMAND_COLOR:
-            Serial.println("Color?");
-            while(!Serial.available());
-            current_color.r = Serial.parseInt();
-            EEPROM.write(2, current_color.r);
-            Serial.println(current_color.r);
-            while(!Serial.available());
-            current_color.g = Serial.parseInt();
-            EEPROM.write(3, current_color.g);
-            Serial.println(current_color.g);
-            while(!Serial.available());
-            current_color.b = Serial.parseInt();
-            Serial.println(current_color.b);
-            EEPROM.write(4, current_color.b);
+            Serial.print("Color? ");
+
+            Serial.print("r: ");
+            current_color.r = read_serial();
+            EEPROM.write(EPROM_R, current_color.r);
+
+            Serial.print(" g: ");
+            current_color.g = read_serial();
+            EEPROM.write(EPROM_G, current_color.g);
+
+            Serial.print(" b: ");
+            current_color.b = read_serial();
+            EEPROM.write(EPROM_B, current_color.b);
+            Serial.println();
             break;
         default:
             Serial.println("Unknown Command");
@@ -121,18 +147,22 @@ void readCommand() {
 }
 
 void printMode() {
+    Serial.print("Mode: ");
     switch (current_mode) {
         case MODE_OFF:
-            Serial.print("MODE: OFF");
+            Serial.print("OFF");
             break;
         case MODE_BLINK_RGB:
-            Serial.print("MODE: BLINK_RGB");
+            Serial.print("BLINK_RGB");
             break;
         case MODE_COLOR:
-            Serial.print("MODE: COLOR");
+            Serial.print("COLOR");
             break;
         case MODE_RUNNING_COLOR:
-            Serial.print("MODE: RUNNING_COLOR");
+            Serial.print("RUNNING_COLOR");
+            break;
+        case MODE_LEFT_RIGHT_TEST:
+            Serial.print("LEFT_RIGHT_TST");
             break;
         default:
             Serial.print("UNKNOWN MODE: ");
@@ -290,4 +320,70 @@ void running_parallel() {
         led.sync();
         delay(current_speed);
     }
+}
+
+void setLEDsInRange(cRGB color, int start, int stop) {
+    for (int i = start; i < stop; i++) {
+        led.set_crgb_at(i, color);
+    }
+    led.sync();
+}
+
+void left_right_test() {
+    int middle_treshold = 40;
+    int manni_treshold = 30;
+
+    cRGB side1, side2;
+
+    BMA::read_all_axes();
+
+    if (BMA::Y > manni_treshold) {
+        //side 1 = pink
+        side1.r = 255;
+        side1.g = 20;
+        side1.b = 150;
+
+        //side 2 = pink
+        side2.r = 255;
+        side2.g = 20;
+        side2.b = 150;
+    } else {
+        if (BMA::X < -middle_treshold) {
+            // side 1 = scaled red
+            side1.r = -BMA::X;
+            side1.g = 0;
+            side1.b = 0;
+
+            // side 2 = off
+            side2.r = 0;
+            side2.g = 0;
+            side2.b = 0;
+
+        } else if (BMA::X > middle_treshold) {
+            // side 1 = off
+            side1.r = 0;
+            side1.g = 0;
+            side1.b = 0;
+
+            // side 2 = scaled green
+            side2.r = 0;
+            side2.g = BMA::X;
+            side2.b = 0;
+        } else {
+            // side 1 = blue
+            side1.r = 0;
+            side1.g = 0;
+            side1.b = 255;
+
+            // side 2 = blue
+            side2.r = 0;
+            side2.g = 0;
+            side2.b = 255;
+        }
+    }
+
+    // side 1: 0 - 14
+    setLEDsInRange(side1, 0, 14);
+    // side 2: 29 - 15
+    setLEDsInRange(side2, 15, 29);
 }
